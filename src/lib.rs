4583a25f05b9a9726993097c475d6f2e65defa3b
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-12-23 19:22:10 +0900

    done

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,37 @@
-use wasm_bindgen::prelude::*;
+// Rust code (e.g., image decoding logic)
+// This example uses the `image` crate
 
+use image::DynamicImage;
+use std::io::Cursor;
 
-#[wasm_bindgen]
-struct Btn{
-    count: u128,
+#[no_mangle]
+pub fn decode_image_from_buffer(buffer_ptr: *const u8, buffer_len: usize) -> *const u8 {
+    // Convert the passed buffer to a Rust slice
+    let buffer = unsafe { std::slice::from_raw_parts(buffer_ptr, buffer_len) };
+
+    // Decode the image from the buffer
+    let img = image::load_from_memory(buffer).unwrap_or_else(|_| {
+        panic!("Failed to decode the image");
+    });
+
+    // Do image processing or manipulation here if needed
+    // For simplicity, we'll just return the decoded image as a buffer
+
+    // Encode the image back into a buffer (JPEG in this case)
+    let mut img_buffer: Vec<u8> = Vec::new();
+    img.write_to(&mut Cursor::new(&mut img_buffer), image::ImageOutputFormat::Png)
+        .expect("Failed to encode image");
+
+    // Return the pointer to the image buffer
+    let ptr = img_buffer.as_ptr();
+    std::mem::forget(img_buffer); // Prevent Rust from freeing the memory
+    ptr
 }
 
-#[wasm_bindgen]
-impl Btn {
-    pub fn new()-> Self{
-        Self{
-            count: 0,
-        }
-    }
-    pub fn counting(mut self)-> Self{
-        self.count+=1;
-        self
-    }
-}
-
-#[wasm_bindgen]
-pub fn clicked_btn_then_counting(btn: Btn) -> Btn {
-    Btn::counting(btn)
+#[no_mangle]
+pub fn get_image_buffer_length() -> usize {
+    // Return the length of the image buffer
+    // Implement this based on your requirements
+    // In this example, we're not keeping track of the buffer length
+    0
 }
--- static/imgage_viewer/WASM_WEB.js
+++ static/imgage_viewer/WASM_WEB.js
@@ -1,80 +1,5 @@
 let wasm;
 
-const cachedTextDecoder = (typeof TextDecoder !== 'undefined' ? new TextDecoder('utf-8', { ignoreBOM: true, fatal: true }) : { decode: () => { throw Error('TextDecoder not available') } } );
-
-if (typeof TextDecoder !== 'undefined') { cachedTextDecoder.decode(); };
-
-let cachedUint8Memory0 = null;
-
-function getUint8Memory0() {
-    if (cachedUint8Memory0 === null || cachedUint8Memory0.byteLength === 0) {
-        cachedUint8Memory0 = new Uint8Array(wasm.memory.buffer);
-    }w
-    return cachedUint8Memory0;
-}
-
-function getStringFromWasm0(ptr, len) {
-    ptr = ptr >>> 0;
-    return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
-}
-
-function _assertClass(instance, klass) {
-    if (!(instance instanceof klass)) {
-        throw new Error(`expected instance of ${klass.name}`);
-    }
-    return instance.ptr;
-}
-/**
-* @param {Btn} btn
-* @returns {Btn}
-*/
-export function clicked_btn_then_counting(btn) {
-    _assertClass(btn, Btn);
-    var ptr0 = btn.__destroy_into_raw();
-    const ret = wasm.btn_counting(ptr0);
-    return Btn.__wrap(ret);
-}
-
-/**
-*/
-export class Btn {
-
-    static __wrap(ptr) {
-        ptr = ptr >>> 0;
-        const obj = Object.create(Btn.prototype);
-        obj.__wbg_ptr = ptr;
-
-        return obj;
-    }
-
-    __destroy_into_raw() {
-        const ptr = this.__wbg_ptr;
-        this.__wbg_ptr = 0;
-
-        return ptr;
-    }
-
-    free() {
-        const ptr = this.__destroy_into_raw();
-        wasm.__wbg_btn_free(ptr);
-    }
-    /**
-    * @returns {Btn}
-    */
-    static new() {
-        const ret = wasm.btn_new();
-        return Btn.__wrap(ret);
-    }
-    /**
-    * @returns {Btn}
-    */
-    counting() {
-        const ptr = this.__destroy_into_raw();
-        const ret = wasm.btn_counting(ptr);
-        return Btn.__wrap(ret);
-    }
-}
-
 async function __wbg_load(module, imports) {
     if (typeof Response === 'function' && module instanceof Response) {
         if (typeof WebAssembly.instantiateStreaming === 'function') {
@@ -109,9 +34,6 @@ async function __wbg_load(module, imports) {
 function __wbg_get_imports() {
     const imports = {};
     imports.wbg = {};
-    imports.wbg.__wbindgen_throw = function(arg0, arg1) {
-        throw new Error(getStringFromWasm0(arg0, arg1));
-    };
 
     return imports;
 }
@@ -123,7 +45,6 @@ function __wbg_init_memory(imports, maybe_memory) {
 function __wbg_finalize_init(instance, module) {
     wasm = instance.exports;
     __wbg_init.__wbindgen_wasm_module = module;
-    cachedUint8Memory0 = null;
 
 
     return wasm;
