ec5da5b1183c45d647820ce8fd3bd81db9320c45
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-12-23 21:24:28 +0900

    WASM done

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,23 @@
-// Rust code (e.g., image decoding logic)
-// This example uses the `image` crate
+// lib.rs (Rust code)
 
-use image::DynamicImage;
-use std::io::Cursor;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::{Document, Element, HtmlImageElement, Window};
 
-#[no_mangle]
-pub fn decode_image_from_buffer(buffer_ptr: *const u8, buffer_len: usize) -> *const u8 {
-    // Convert the passed buffer to a Rust slice
-    let buffer = unsafe { std::slice::from_raw_parts(buffer_ptr, buffer_len) };
+#[wasm_bindgen]
+pub fn add_image(src: &str, alt: &str) -> Result<(), JsValue> {
+    // Get window, document, and body elements
+    let window = web_sys::window().ok_or("No window found")?;
+    let document = window.document().ok_or("No document found")?;
+    let body = document.body().ok_or("No body found")?;
 
-    // Decode the image from the buffer
-    let img = image::load_from_memory(buffer).unwrap_or_else(|_| {
-        panic!("Failed to decode the image");
-    });
+    // Create img element
+    let img = document.create_element("img")?.unchecked_into::<HtmlImageElement>();
+    img.set_src(src);
+    img.set_alt(alt);
 
-    // Do image processing or manipulation here if needed
-    // For simplicity, we'll just return the decoded image as a buffer
-
-    // Encode the image back into a buffer (JPEG in this case)
-    let mut img_buffer: Vec<u8> = Vec::new();
-    img.write_to(&mut Cursor::new(&mut img_buffer), image::ImageOutputFormat::Png)
-        .expect("Failed to encode image");
-
-    // Return the pointer to the image buffer
-    let ptr = img_buffer.as_ptr();
-    std::mem::forget(img_buffer); // Prevent Rust from freeing the memory
-    ptr
-}
-
-#[no_mangle]
-pub fn get_image_buffer_length() -> usize {
-    // Return the length of the image buffer
-    // Implement this based on your requirements
-    // In this example, we're not keeping track of the buffer length
-    0
+    // Append img to the body
+    body.append_child(&img)?;
+    
+    Ok(())
 }
--- static/imgage_viewer/script.js
+++ static/imgage_viewer/script.js
@@ -1,32 +1,20 @@
-window.onload = function() {
-    const kindWrap =  document.querySelector('.kind_wrap');
-    const slider = kindWrap.querySelector('.slider');
-    const slideLis = slider.querySelectorAll('li')
-    const moveButton = kindWrap.querySelector('.arrow');
-  
-    const liWidth = slideLis[0].clientWidth;
-    const sliderWidth = liWidth * slideLis.length;
-    slider.style.width = `${sliderWidth}px` ;
-  
-    let currentIdx = 0; 
-    let translate = 0; 
-    moveButton.addEventListener('click', moveSlide);
-  
-    function moveSlide(event) {
-      event.preventDefault();
-      if (event.target.className === 'next') {
-        if (currentIdx !== slideLis.length -1) {
-          translate -= liWidth;
-          slider.style.transform = `translateX(${translate}px)`;
-          currentIdx += 1;
-        }
-      } else if (event.target.className === 'prev') {
-          if (currentIdx !== 0) {
-            translate += liWidth;
-            slider.style.transform = `translateX(${translate}px)`;
-            currentIdx -= 1;
-          }
-        }
+// script.js
+
+import init, { add_image } from './WASM_WEB.js';
+
+// Initialize the WASM module
+async function run() {
+    await init();
+
+    // Call the Rust function to add an image dynamically
+    try {
+        add_image('./image/1.png', 'Dynamic Image');
+        add_image('./image/2.png', 'Dynamic Image');
+        add_image('./image/3.png', 'Dynamic Image');
+        add_image('./image/4.png', 'Dynamic Image');
+    } catch (error) {
+        console.error('Error adding image:', error);
     }
-  
-  }
+}
+
+run();
